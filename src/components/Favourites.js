82d4d19aceb37b38b1b364f5ea70bd128e61503a
daniel kelly <danieljkelly91@gmail.com>
2018-04-30 11:37:15 +0100

    installed google-map-react, got map to display in favourites

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,9 +1,11 @@
 import React from "react";
+import MapContainer from '../containers/MapContainer';
 
 const Favourites = () => (
   <div>
     <h4>Favourites</h4>
     <p>Welcome to our magical favourites page</p>
+  <MapContainer />
   </div>
 );
 
