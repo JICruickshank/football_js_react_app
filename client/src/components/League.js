4541239ee900502f626d038ed900aeccf0d1c34f
neifirst <e.mcculloch.arch@gmail.com>
2018-04-30 15:21:19 +0100

    manual merge

--- client/src/components/League.js
+++ client/src/components/League.js
@@ -1,10 +1,51 @@
 import React from "react";
-
-const League = () => (
-  <div>
-    <h4>League</h4>
-    <p>Welcome to our magical league page</p>
-  </div>
-);
-
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+// import '../css/Table.css';
+// import "../../node_modules/react-bootstrap-table/css/react-bootstrap-table.css";
+class League extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      league: []
+    }
+  }
+  componentDidMount() {
+    fetch("http://api.football-data.org/v1/competitions/445/leagueTable?api-key=ce59c6fd7c2d47c29fb4c133e01112d8", {headers: {'x-auth-token': 'ce59c6fd7c2d47c29fb4c133e01112d8'}})
+      .then(response => response.json())
+      .then(json => this.setState({league: json.standing}))
+  }
+  render() {
+  let data = [];
+  for (let team of this.state.league) {
+    const teamData = {
+      position: team.positon,
+      name: team.teamName,
+      played: team.playedGames,
+      won: team.wins,
+      drawn: team.draws,
+      lost: team.losses,
+      for: team.goals,
+      against: team.goalsAgainst,
+      points: team.points,
+      goalDifference: team.goalDifference}
+      data.push(teamData);
+  }
+  return (
+    <div>
+      <BootstrapTable data={data}>
+        <TableHeaderColumn dataField="position">Position</TableHeaderColumn>
+        <TableHeaderColumn dataField="name" isKey>Team</TableHeaderColumn>
+        <TableHeaderColumn dataField="played">P</TableHeaderColumn>
+        <TableHeaderColumn dataField="won">W</TableHeaderColumn>
+        <TableHeaderColumn dataField="drawn">D</TableHeaderColumn>
+        <TableHeaderColumn dataField="lost">L</TableHeaderColumn>
+        <TableHeaderColumn dataField="for">F</TableHeaderColumn>
+        <TableHeaderColumn dataField="against">A</TableHeaderColumn>
+        <TableHeaderColumn dataField="points">Pts</TableHeaderColumn>
+        <TableHeaderColumn dataField="goalDifference">GD</TableHeaderColumn>
+      </BootstrapTable>
+    </div>
+  )
+  }
+}
 export default League;
---
+++ client/src/containers/MapContainer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import Favourites from '../components/Favourites';
+class MapContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.map = null;
+        this.maps = null;
+        this.renderMarker = this.renderMarker.bind(this);
+        this.state = {
+            markers: []
+        };
+        this.storeMaps = this.storeMaps.bind(this);
+    }
+    storeMaps({map, maps}){
+        this.map = map;
+        this.maps = maps;
+        this.renderMarker();
+    }
+    renderMarker(){
+        console.log("In renderMarker");
+        let testMarker = new this.maps.Marker({
+            position: this.props.marker,
+            map: this.map
+        });
+    }
+    componentDidUpdate(prevProps, prevState, snapshot){
+        this.renderMarker();
+        // let marker = this.props.marker;
+    }
+    render(){
+        return(
+            <div style={{ height: `300px`, width: '90%' }}>
+                <GoogleMapReact
+                    center = { { lat: 52.489471, lng: -1.898575 } }
+                    zoom = { 6 }
+                    onGoogleApiLoaded = {this.storeMaps}
+                />
+            </div>
+        )
+    }
+}
+export default MapContainer;
