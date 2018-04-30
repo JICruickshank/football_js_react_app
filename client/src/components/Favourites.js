8f2ca044d45f5cb4de5408479a6c45f4b42c0530
daniel kelly <danieljkelly91@gmail.com>
2018-04-30 18:44:08 +0100

    updated favourites.js and mapContainer.js, markers now added to all stadiums

--- client/src/components/Favourites.js
+++ client/src/components/Favourites.js
@@ -1,11 +1,34 @@
 import React from "react";
 import MapContainer from '../containers/MapContainer';
-const testMarker = {lat: 53.4831, lng: -2.2004}
+
+const locations = [
+    {team: "Manchester City FC", coords:{lat: 53.4831, lng: -2.2004}},
+    {team: "Manchester United FC", coords:{lat: 53.4631, lng: -2.2913}},
+    {team: "Liverpool FC", coords:{lat: 53.4308, lng: -2.9608}},
+    {team: "Tottenham FC", coords:{lat: 51.5560, lng: -0.2795}},
+    {team: "Chelsea FC", coords:{lat: 51.4817, lng: -0.1910}},
+    {team: "Arsenal FC", coords:{lat: 51.5549, lng: -0.1084}},
+    {team: "Burnley FC", coords:{lat: 53.7890, lng: -2.2302}},
+    {team: "Everton FC", coords:{lat: 53.4388, lng: -2.9663}},
+    {team: "Leicester City FC", coords:{lat: 52.6204, lng: -1.1422}},
+    {team: "Newcastle United FC", coords:{lat: 54.9756, lng: -1.6217}},
+    {team: "Crystal Palace FC", coords:{lat: 51.398270, lng: -0.085644}},
+    {team: "Watford FC", coords:{lat: 51.649939, lng: -0.401707}},
+    {team: "AFC Bournemouth", coords:{lat: 50.7352, lng: -1.838515}},
+    {team: "Brighton & Hove Albion", coords:{lat: 50.861701, lng: -0.084070}},
+    {team: "West Ham United FC", coords:{lat: 51.5387078, lng: -0.0175076}},
+    {team: "Huddersfield Town", coords:{lat: 53.654516, lng: -1.768398}},
+    {team: "Swansea City FC", coords:{lat: 51.642827, lng: -3.934255}},
+    {team: "Southampton FC", coords:{lat: 50.905024, lng: -1.3923383}},
+    {team: "Stoke City FC", coords:{lat: 52.988343, lng: -2.175693}},
+    {team: "West Bromwich Albion FC", coords:{lat: 52.509058, lng: -1.964110}}
+];
+
 const Favourites = () => (
   <div>
     <h4>Favourites</h4>
     <p>Welcome to our magical favourites page</p>
-  <MapContainer marker={testMarker}/>
+  <MapContainer locations={locations}/>
   </div>
 );
 export default Favourites;
