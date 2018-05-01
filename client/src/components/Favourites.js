847a672a99d1b5a9b65a52f28e44aa6af4908343
neifirst <e.mcculloch.arch@gmail.com>
2018-05-01 17:11:12 +0100

    app now fetches from server to provide info in state

--- client/src/components/Favourites.js
+++ client/src/components/Favourites.js
@@ -1,28 +1,7 @@
 import React from "react";
 import MapContainer from '../containers/MapContainer';
 
-const locations = [
-    {team: "Manchester City FC", coords:{lat: 53.4831, lng: -2.2004}, description: "Eithad Stadium"},
-    {team: "Manchester United FC", coords:{lat: 53.4631, lng: -2.2913}, description: "Old Trafford"},
-    {team: "Liverpool FC", coords:{lat: 53.4308, lng: -2.9608}, description: "Anfield"},
-    {team: "Tottenham FC", coords:{lat: 51.5560, lng: -0.2795}, description: "Wembley"},
-    {team: "Chelsea FC", coords:{lat: 51.4817, lng: -0.1910}, description: "Stamford Bridge"},
-    {team: "Arsenal FC", coords:{lat: 51.5549, lng: -0.1084}, description: "The Emirates Stadium"},
-    {team: "Burnley FC", coords:{lat: 53.7890, lng: -2.2302}, description: "Turf Moor"},
-    {team: "Everton FC", coords:{lat: 53.4388, lng: -2.9663}, description: "Goodison Park"},
-    {team: "Leicester City FC", coords:{lat: 52.6204, lng: -1.1422}, description: "King Power Stadium"},
-    {team: "Newcastle United FC", coords:{lat: 54.9756, lng: -1.6217}, description: "St James' Park"},
-    {team: "Crystal Palace FC", coords:{lat: 51.398270, lng: -0.085644}, description: "Selhurst Park"},
-    {team: "Watford FC", coords:{lat: 51.649939, lng: -0.401707}, description: "Vicarage Road"},
-    {team: "AFC Bournemouth", coords:{lat: 50.7352, lng: -1.838515}, description: "Dean Court"},
-    {team: "Brighton & Hove Albion", coords:{lat: 50.861701, lng: -0.084070}, description: "Falmer Stadium"},
-    {team: "West Ham United FC", coords:{lat: 51.5387078, lng: -0.0175076}, description: "London Stadium"},
-    {team: "Huddersfield Town", coords:{lat: 53.654516, lng: -1.768398}, description: "Kirklees Stadium"},
-    {team: "Swansea City FC", coords:{lat: 51.642827, lng: -3.934255}, description: "Liberty Stadium"},
-    {team: "Southampton FC", coords:{lat: 50.905024, lng: -1.3923383}, description: "St Mary's Stadium"},
-    {team: "Stoke City FC", coords:{lat: 52.988343, lng: -2.175693}, description: "Bet 365 Stadium"},
-    {team: "West Bromwich Albion FC", coords:{lat: 52.509058, lng: -1.964110}, description: "The Hawthorns"}
-];
+
 
 const Favourites = () => (
   <div>
