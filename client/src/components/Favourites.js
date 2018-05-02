e2e45bb3fef6d90821fd9198337d0cf4e04d9b5d
daniel kelly <danieljkelly91@gmail.com>
2018-05-02 15:12:57 +0100

    adds deleteFromFavourites method in Favourites.js and adds res.header to allow DELETE method to resolve issue with access control

--- client/src/components/Favourites.js
+++ client/src/components/Favourites.js
@@ -5,12 +5,29 @@ import MapContainer from '../containers/MapContainer';
 class Favourites extends React.Component {
   constructor(props) {
     super(props);
+    this.deleteFromFavourites = this.deleteFromFavourites.bind(this);
     this.checkTeam = this.checkTeam.bind(this);
     this.state = {
       favourites: [],
       locations: []
     }
   }
+
+  deleteFromFavourites(id_passed_in){
+        debugger;
+        const request = new XMLHttpRequest();
+        request.open("DELETE", "//localhost:3001/api/favourites/" + id_passed_in);
+        debugger;
+        request.setRequestHeader("Content-Type", "application/json");
+        request.addEventListener('load', function(){
+          if(this.status !== 201){
+            return;
+          }
+        })
+        request.send();
+      }
+
+
 
   checkTeam(){
     let array =[]
@@ -38,7 +55,7 @@ class Favourites extends React.Component {
         {this.props.favourites.map((favourite, index) => {
           return (
             <React.Fragment>
-              <p>{favourite.date} {favourite.homeTeam} {favourite.homeGoals}-{favourite.awayGoals} {favourite.awayTeam} <button>Delete Fixture</button></p>
+              <p>{favourite.date} {favourite.homeTeam} {favourite.homeGoals}-{favourite.awayGoals} {favourite.awayTeam} <button onClick={() => this.deleteFromFavourites(favourite._id)}>Delete Fixture</button></p>
             </React.Fragment>
           )
         })}
--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const ObjectID = require("mongodb").ObjectID;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
